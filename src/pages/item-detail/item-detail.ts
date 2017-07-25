22b86ffeda810b0e08fcf8d6ac9588517d8c0aff
Michael Reda <michaelreda96@gmail.com>
2017-07-25 14:57:11 +0200

    company profile edits
    - avatar in cards
    - price
    - apply button
    - message

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { AlertController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 
 import { Items } from '../../providers/providers';
 import {Http} from '@angular/http';
@@ -17,15 +18,18 @@ export class ItemDetailPage {
 	company_details: any ={};
 	company_simulations: any =[];
 	refresher:any;
+	loader:any={};
 
-	constructor(public navCtrl: NavController, navParams: NavParams, items: Items, http: Http) {
+	constructor(public navCtrl: NavController, navParams: NavParams, items: Items, http: Http, public alertCtrl:AlertController,public loadingCtrl: LoadingController) {
 		this.item = navParams.get('item') || items.defaultItem;
 		this.loading=true;
 
+		this.presentLoading();
 		http.get("https://walidpc.eu-gb.mybluemix.net/company_details?company_id=1").subscribe(data => {
 			var res = JSON.parse(data['_body']);
 			this.company_details=res;
 			console.log(this.company_details);
+			this.loader.dismiss();
 			// this.loading=false;
 		});
 		http.get("https://walidpc.eu-gb.mybluemix.net/get_company_simulations?company_id=1").subscribe(data => {
@@ -48,5 +52,40 @@ export class ItemDetailPage {
 				this.refresher.complete();
 		}
 
+		open_email_content_popup(){
+			let alert = this.alertCtrl.create({
+				title: 'Message',
+				inputs: [
+					{
+						name: 'message',
+						placeholder: 'enter your message here',
+						type: 'text'
+					}
+				],
+				buttons: [
+					{
+						text: 'Cancel',
+						role: 'cancel',
+						handler: data => {
+							console.log('Cancel clicked');
+						}
+					},
+					{
+						text: 'Send',
+						handler: data => {
+							console.log(data.message);
+						}
+					}
+				]
+			});
+			alert.present();
+		}
 
+
+		presentLoading() {
+			this.loader = this.loadingCtrl.create({
+				content: "Please wait..."
+			});
+			this.loader.present();
+		}
 }
