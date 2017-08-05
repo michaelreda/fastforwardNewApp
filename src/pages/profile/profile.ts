c58954eb76559577eb2ff4c0363b2419d7ece432
Michael Reda <michaelreda96@gmail.com>
2017-08-05 17:53:52 +0200

    Add rating stars in the student profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -19,14 +19,14 @@ export class Profile {
 	refresher:any;
 
 	constructor(public navCtrl: NavController, navParams: NavParams, http: Http, public alertCtrl:AlertController,public loadingCtrl: LoadingController,public actionSheetCtrl: ActionSheetController) {
-		http.get("https://walidpc.eu-gb.mybluemix.net/user_info?id=1").subscribe(data => {
+		http.get("https://walidpc.eu-gb.mybluemix.net/user_info?id=36").subscribe(data => {
 			var res = JSON.parse(data['_body']);
 			this.user_info=res;
 			console.log(this.user_info);
 			//this.loading=false;
 		});
 
-		http.get("https://walidpc.eu-gb.mybluemix.net/user_simulations?id=1").subscribe(data => {
+		http.get("https://walidpc.eu-gb.mybluemix.net/user_simulations?id=36").subscribe(data => {
 			var res = JSON.parse(data['_body']);
 			this.user_simulations=res;
 			console.log(this.user_simulations);
