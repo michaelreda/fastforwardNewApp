6f5c0b728cbb714dffaa40f0244cd2ba9c292792
Michael Reda <michaelreda96@gmail.com>
2017-08-10 18:57:44 +0200

    add video popup bug resolved

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import{Vedios} from'../vedios/vedios';
-import {  Profile} from "../profile/profile";
+import { Vedios } from '../vedios/vedios';
+import { Profile } from "../profile/profile";
 import { ActionSheetController, Platform } from 'ionic-angular';
 import { NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
-import {Http} from '@angular/http';
+import { Http } from '@angular/http';
 
 
 
@@ -18,25 +18,25 @@ export class TabsPage {
 	tab1Root = AboutPage;
 	tab2Root = Vedios;
 	tab3Root = Profile;
-	ios:boolean=false;
+	ios: boolean = false;
 	root;
-	constructor(public actionSheetCtrl: ActionSheetController,public navCtrl: NavController, navParams: NavParams,public alertCtrl:AlertController,public http: Http,public plt: Platform) {
-		this.root=this.tab1Root;
-		if(plt.is('ios')){
-			this.ios=true;
+	constructor(public actionSheetCtrl: ActionSheetController, public navCtrl: NavController, navParams: NavParams, public http: Http, public plt: Platform,public alertCtrl: AlertController) {
+		this.root = this.tab1Root;
+		if (plt.is('ios')) {
+			this.ios = true;
 		}
 	}
-	switch1(){
-			this.root=this.tab1Root;
+	switch1() {
+		this.root = this.tab1Root;
 	}
-	switch2(){
-		this.root=this.tab2Root;
+	switch2() {
+		this.root = this.tab2Root;
 	}
-	switch3(){
-			this.root=this.tab3Root;
+	switch3() {
+		this.root = this.tab3Root;
 	}
-	switch4(){
-			this.presentAdminActionSheet();
+	switch4() {
+		this.presentAdminActionSheet();
 	}
 
 	presentAdminActionSheet() {
@@ -58,7 +58,9 @@ export class TabsPage {
 				{
 					text: 'Add Video',
 					handler: () => {
-							this.show_add_video_popup();
+						setTimeout(() => {
+						this.show_add_video_popup();
+						},100);
 					}
 				},
 				{
@@ -74,8 +76,8 @@ export class TabsPage {
 		actionSheet.present();
 	}
 
-	show_add_video_popup(){
-		let alert = this.alertCtrl.create({
+	show_add_video_popup() {
+		this.alertCtrl.create({
 			title: 'Add video to your media feed',
 			inputs: [
 				{
@@ -100,23 +102,25 @@ export class TabsPage {
 				{
 					text: 'Add',
 					handler: data => {
-					var video = {
-						"video_link":data.video_link,
-						"description":data.video_description,
-						"company_id":"5"
-					}
-					console.log(data.video_link);
-					this.http.post("https://ffserver.eu-gb.mybluemix.net/add-video",video).subscribe(data => {
-						var res = JSON.parse(data['_body']);
-						// this.user_simulations=res;
-						console.log(res);
-						// this.loading=false;
-					});
+						var video = {
+							"video_link": data.video_link,
+							"description": data.video_description,
+							"company_id": "5"
+						}
+						console.log(data.video_link);
+						this.http.post("https://ffserver.eu-gb.mybluemix.net/add-video", video).subscribe(data => {
+							var res = JSON.parse(data['_body']);
+							// this.user_simulations=res;
+							console.log(res);
+							// this.loading=false;
+						});
 
 					}
 				}
 			]
-		});
-		alert.present();
+		}).present();
+		
+		
+
 	}
 }
