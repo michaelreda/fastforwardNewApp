e75ece9c0dca171f4ed38ad55cc872943dce5b66
TheMaestro9 <walidmoussa995@gmail.com>
2017-08-31 17:49:00 +0200

    app.component fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,50 @@
-import { TutorialPage } from './../pages/tutorial/tutorial';
+
 import { VersionCheckPage } from './../pages/version-check/version-check';
 import { TabsPage } from './../pages/tabs/tabs';
 import { Component } from '@angular/core';
-import { Platform, NavController, LoadingController} from 'ionic-angular';
+import { Platform, LoadingController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 import { TimerPage } from '../pages/timer/timer';
 import { Storage } from '@ionic/storage';
-import {Registerform} from '../pages/registerform/registerform';
 import { DataService } from '../providers/data-service';
 import { Network } from '@ionic-native/network';
 import { PaymentMethodPage } from '../pages/payment-method/payment-method';
-
+import {  TutorialPage } from '../pages/tutorial/tutorial';
 @Component({
   templateUrl: 'app.html',
-  
-  
+
+
 })
 
 
 export class MyApp {
- rootPage:any ;
+  rootPage: any;
   id;
   timer;
-date;
-nextpage;
+  date;
+  nextpage;
   check;
 
   connection_error_popup: any;
-  constructor(platform: Platform, statusBar: StatusBar, private loadingCtrl: LoadingController, splashScreen: SplashScreen, private store: Storage, private DS: DataService,private network: Network) {
+  constructor(platform: Platform, statusBar: StatusBar, private loadingCtrl: LoadingController, splashScreen: SplashScreen, private DS: DataService, private network: Network, public store: Storage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-    
-
     });
-  
- //this.rootPage=TimerPage;
 
+    //this.rootPage=TabsPage;
 
     store.get('user_id').then((val) => {
       console.log('store', val);
       this.DS.seturl("https://ffserver.eu-gb.mybluemix.net/check-version?version=1");
       this.DS.load().subscribe(
-        data => (this.handelResponse(data ,val))
+        data => (this.handelResponse(data, val))
       );
-      
+
     });
 
   }
@@ -68,16 +63,19 @@ nextpage;
     });
   }
 
-  handelResponse(data , user_id) {
+  handelResponse(data, user_id) {
     this.check = data.result;
     console.log(this.check);
     if (!this.check) {
       // this.rootPage = VersionCheckPage;
-      this.rootPage= VersionCheckPage;
+      this.rootPage = VersionCheckPage;
     }
     if (user_id == null || user_id == "") {
-        this.rootPage= LoginPage;
-      }
+      this.rootPage = LoginPage;
+    }
+    else {
+      if (user_id == 324)
+        this.rootPage = TabsPage;
       else {
         if (user_id==324) 
             this.rootPage=TabsPage;
@@ -102,8 +100,24 @@ nextpage;
             
        }
       }
+    }
 
   }
 
+
+  // checkTimer (callback : (TimerWorking : boolean) => void){
+
+  //   this.DS.seturl("https://ffserver.eu-gb.mybluemix.net/check-date-test");
+  //       this.DS.load().subscribe(
+  //           data =>{
+  //             console.log("hello" , data.result); 
+  //             callback (data.result) ; 
+  //             });
+
+
+
+
+
+  // }
 }
 
